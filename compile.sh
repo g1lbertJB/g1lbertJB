bbc32082bb55c74194524652a27a5dba303989ef
LukeeGD <26163116+LukeZGD@users.noreply.github.com>
2026-09-01 16:36:01 +0800

    update compile stuff 2

--- compile.sh
+++ compile.sh
@@ -33,7 +33,7 @@ if [[ $(uname) == "Darwin" ]]; then
     if [[ ! -d limd ]]; then
         mkdir limd
         pushd limd
-        curl -LO https://gist.github.com/LukeZGD/0f5ba45494912c419f59bd8178ab57bd/raw/d73d73de1b786dd1e590a7baac3f2580eb2a0d57/limd-build-macos.sh
+        curl -LO https://gist.github.com/LukeZGD/0f5ba45494912c419f59bd8178ab57bd/raw/30f1990ff28a3ccf78ec712019b1e107cd9300e5/limd-build-macos.sh
         chmod +x limd-build-macos.sh
         ./limd-build-macos.sh
         popd
